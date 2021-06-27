a39acbf73f45428f3c85d398808b89b553f7f3c5
heo_minkyu <79818840+heo-mk@users.noreply.github.com>
2021-06-28 03:19:20 +0900

    리액트 폴더 파일 재조정

---
+++ react_prac_playground/02_advancedInfos/07_HigherOrderComponent.js
@@ -0,0 +1,2 @@
+// 고차컴포넌트 === 컴포넌트를 가져와서 새 컴포넌트를 반환하는 함수
+const EnhancedComponent = higherOrderComponent(WrappedComponent);
--- react_prac_playground/hooksPreviewRef.js
+++ react_prac_playground/hooksPreviewRef.js
@@ -1,2 +1,50 @@
 const [file, setFile] = useState('');
-const [previewURL, setPreviewURL] = useState
+const [previewURL, setPreviewURL] = useState('');
+const [preview, setPreview] = useState(null);
+const fileRef = useRef();
+
+useEffect(() => {
+  if (file !== '') // 처음 파일을 등록하지 않았을 때를 방지.
+    setPreview(<img className="img_preview" src={previewURL}></img>);
+  return () => {
+  }
+}, [previewURL])
+
+const handleFileOnChange = (event) => {
+  event.preventDefault();
+  let file = event.target.files[0];
+  let reader = new FileReader();
+
+  reader.onloadend = (e) => {
+    setFile(file);
+    setPreviewURL(reader.result);
+  }
+  if (file) {
+    reader.readAsDataURL(file);
+  }
+  const handleFileButtonClick = (e) => {
+    e.preventDefault();
+    fileRef.current.click();
+  }
+
+  return (
+    <>
+      <div className = 'preview-rapping'>
+        {preview}
+      </div>
+      <aside className = 'side'>
+        <input ref={fileRef} hidden = {true} id = "file" type = 'file' onChange={handleFileOnChange}></input>
+        <header className='side-header'>
+          <Typography align="center" variant='overline' display='block' gutterButtom>
+            Title text
+          </Typography>
+        </header>
+        <div style={{padding:10}}>
+          <button onClick={handleFileButtonClick}>Upload</button>
+        </div>
+      </aside>
+    </>  
+  )
+
+
+}
